4ebccab028f415a1d3f540f6cb4f7201e3a8c11f
Dorian <dkodesch8292@student.pps.net>
2022-10-21 08:40:09 -0700

    parallelogram vectors to vertices conversion

--- Assets/Rotation/Rotors3d.cs
+++ Assets/Rotation/Rotors3d.cs
@@ -1,7 +1,14 @@
-// 3d testing for rotors just to make sure I understand this
+// rotating 
 using UnityEngine;
 
 public class Rotors3d : MonoBehaviour
 {
-    
+    Vector3[] ParallelogramVectorsToVertices (
+        Vector3 a, Vector3 b
+    ) {
+        return new Vector3[4]
+        {
+            Vector3.zero, a, b, a + b
+        };
+    }
 }
