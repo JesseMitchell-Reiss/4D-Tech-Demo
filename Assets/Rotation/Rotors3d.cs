58dec4392d871c1dc336a9daa69d4d36d402f65f
Dorian <dkodesch8292@student.pps.net>
2022-10-24 09:28:32 -0700

    wedge product

--- Assets/Rotation/Rotors3d.cs
+++ Assets/Rotation/Rotors3d.cs
@@ -1,14 +1,15 @@
-// rotating 
+// rotating in 3d to test the math in a more intuitive setting
 using UnityEngine;
 
 public class Rotors3d : MonoBehaviour
 {
-    Vector3[] ParallelogramVectorsToVertices (
-        Vector3 a, Vector3 b
-    ) {
-        return new Vector3[4]
-        {
-            Vector3.zero, a, b, a + b
-        };
-    }
+    /* defining the parallelogram two vectors form by unit planes
+     * coordinates in yz plane, xz plane, and xy plane in that order
+     * because they're respectively parallel to the x, y, z axes */
+    Vector3 WedgeProduct(Vector3 a, Vector3 b) =>
+        new Vector3(a.y * b.z - b.y * a.z, a.x * b.z - b.x * a.z, a.x * b.y - b.x * a.y);
+
+    // gets the vertices of the parallelogram two vectors form in local position
+    Vector3[] ParallelogramVectorsToVertices(Vector3 a, Vector3 b) =>
+        new Vector3[4] {Vector3.zero, a, b, a + b};
 }
