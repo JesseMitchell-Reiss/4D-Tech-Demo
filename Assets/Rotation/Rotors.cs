3307592a6c6e86033ca5bebe60a4703532a5f3c3
Dorian <dkodesch8292@student.pps.net>
2022-12-05 08:54:27 -0800

    more code cleanup

--- Assets/Rotation/Rotors.cs
+++ Assets/Rotation/Rotors.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 
 using static UnityEngine.Mathf;
 
+// represents the parallelogram two vectors make
 public class Bivector4
 {
+    // components are in terms of unit planes
     float WX, YW, WZ, XY, YZ, ZX;
 
     public float wx { get => WX; } 
@@ -14,27 +16,33 @@ public class Bivector4
     public float yz { get => YZ; }
     public float zx { get => ZX; }
 
+    // constructor
     public Bivector4(float wx, float yw, float wz, float xy, float yz, float zx)
     {
         WX = wx; YW = yw; WZ = wz; XY = xy; YZ = yz; ZX = zx;
     }
 
+    // multiplication
     public static Bivector4 operator *(float a, Bivector4 b) =>
         new Bivector4(a * b.wx, a * b.yw, a * b.wz, a * b.xy, a * b.yz, a * b.zx);
 
     public static Bivector4 operator * (Bivector4 b, float a) =>
         new Bivector4(a * b.wx, a * b.yw, a * b.wz, a * b.xy, a * b.yz, a * b.zx);
 
+    // zero bivector
     public static Bivector4 zero { get => new Bivector4(0f, 0f, 0f, 0f, 0f, 0f); }
 
+    // normalization
     public Bivector4 normalized { get => this * (1f / magnitude); }
 
     public float magnitude
     { 
         get => 
-            Sqrt(WX * WX + YW * YW + WZ * WZ + XY * XY + YZ * YZ + ZX * ZX); 
+            Sqrt(wx * wx + yw * yw + wz * wz + xy * xy + yz * yz + zx * zx); 
     }
 
+    // wedge product creates the bivector associated with two vectors
+    // noncommunative, order is important
     public static Bivector4 Wedge(Vector4 a, Vector4 b) => new Bivector4(
         a.w * b.x - b.w * a.x, a.y * b.w - b.y * a.w, a.w * b.z - b.w * a.z,
         a.x * b.y - b.x * a.y, a.y * b.z - b.y * a.z, a.z * b.x - b.z * a.x
@@ -47,7 +55,6 @@ public class Rotor4
     // scalar value (contains cosine of angle)
     float A;
 
-    // plane of the vectors (contains sine of angle)
     // providing both the bivector form and the individual coordinates for convenience
     Bivector4 B;
     float WX, YW, WZ, XY, YZ, ZX;
@@ -61,6 +68,7 @@ public class Rotor4
     public float yz { get => YZ; }
     public float zx { get => ZX; }
 
+    // constructors (again, providing bivector and component input options)
     public Rotor4(float a, Bivector4 b)
     {
         A = a; B = b; WX = b.wx; YW = b.yw; WZ = b.wz; XY = b.xy; YZ = b.yz; ZX = b.zx;
@@ -72,75 +80,36 @@ public class Rotor4
         WX = wx; YW = yw; WZ = wz; XY = xy; YZ = yz; ZX = zx;
     }
 
-    public static Rotor4 operator *(Rotor4 r, float s) =>
-        new Rotor4(s * r.a, s * r.b);
-
-    public static Rotor4 operator *(float s, Rotor4 r) =>
-        new Rotor4(s * r.a, s * r.b);
-
-    // rotor-rotor product for combined rotations
-    public static Rotor4 operator * (Rotor4 p, Rotor4 q)
-    {
-        // dot product, sorta
-        float a =
-            p.a * q.a - p.wx * q.wx - p.yw * q.yw - p.wz * q.wz -
-            p.xy * q.xy - p.yz * p.yz - p.zx * p.zx;
-
-        // wedge product, also sorta
-        float wx =
-            p.wx * q.a + q.wx * p.a -
-            p.xy * q.yw + q.xy * p.yw -
-            p.zx * q.wz + q.zx * p.wz;
-
-        float yw =
-            p.yw * q.a + q.yw * p.a -
-            p.wx * q.xy + q.wx * p.xy +
-            p.wz * q.yz - p.yz * q.wz;
-
-        float wz =
-            p.wz * q.a + q.wz * p.a +
-            p.zx * q.wx - q.zx * p.wx +
-            p.yz * q.yw - q.yz * p.yw;
-
-        float xy =
-            p.xy * q.a + q.xy * p.a -
-            p.yw * q.wx + q.yw * p.wx -
-            p.zx * q.yz + q.zx * p.yz;
-
-        float yz =
-            p.yz * q.a + q.yz * p.a -
-            p.wz * q.yw + q.wz * p.yw -
-            p.zx * q.xy + q.zx * p.xy;
-
-        float zx =
-            p.zx * q.a + q.zx * p.a +
-            p.wx * q.wz - q.wx * p.wz -
-            p.xy * q.yz + q.xy * p.yz;
-
-        return new Rotor4(a, wx, yw, wz, xy, yz, zx);
-    }
-
-    public float magnitude { get => Sqrt(A * A + b.magnitude); }
-
+    // multiplication
+    public static Rotor4 operator *(Rotor4 r, float s) => new Rotor4(s * r.a, s * r.b);
+    public static Rotor4 operator *(float s, Rotor4 r) => new Rotor4(s * r.a, s * r.b);
+    
+    // normalization
+    public float magnitude { get => Sqrt(a * a + b.magnitude * b.magnitude); }
     public Rotor4 normalized { get => this * (1f / magnitude); }
 
+    // zero rotor
     public Rotor4 zero { get => new Rotor4(0f, Bivector4.zero); }
 
-    // constructs and applies a rotor
+    // constructs rotor from v1 and v2, applies it to vFrom (t is for interpolation)
     public static Vector4 Rotate(Vector4 v1, Vector4 v2, Vector4 vFrom, float t = 1f)
     {
-        // normalizing
+        // getting associated unit vectors
         Vector4 u1 = v1.normalized;
         Vector4 u2 = v2.normalized;
         Vector4 uFrom = vFrom.normalized;
 
+        // construction and application
         Rotor4 r = ConstructRotor(u1, u2);
         Rotor4 interpolatedRotor = InterpolateRotor(r, t);
         Vector4 v = ApplyRotor(interpolatedRotor, uFrom) * v2.magnitude;
         return v;
     }
-
+    
     // interpolates rotor, r must be normalized and t must be between 0 and 1
+    /* basically get angle of the rotation represented by the rotor
+     * then construct a new rotor with angle = previous angle * t 
+     */
     static Rotor4 InterpolateRotor(Rotor4 r, float t)
     {
         // rotation angle
@@ -153,12 +122,13 @@ public class Rotor4
         return new Rotor4(cosT, b);
     }
 
-    // rotates a vector by a rotor
-    // ab: bivector, u.a * v.b - u.b * v.a
-    // abc: trivector, (r.bc * v.a) + (r.ca * v.b) + (r.ab * v.c)
+    // ab: bivector, u.a * v.b - u.b * v.a where a and b are components (ab = -ba)
+    // abc: trivector, (r.bc * v.a) + (r.ca * v.b) + (r.ab * v.c) where a, b, c are components
     // last row of left matrix gives quadvector (4-volume)
     /* left matrix * vector produces a row 5-vector which gets multipled
      * w/ columns of right matrix */
+
+    // total multipication is:
     /* |  a   wx  -yw   wz | | w | |  a   -wx   yw   wz |
      * | -wx  a    xy  -zx | | x | |  wx   a   -xy   zx |
      * |  yw -xy   a    yz | | y | | -yw   xy   a   -yz |
@@ -181,37 +151,37 @@ public class Rotor4
         //     wxy          xyw          ywx
         tv.z = r.xy * v.w + r.yw * v.x + r.wx * v.y;
 
+        /* |  a   wx  -yw   wz | | w |
+         * | -wx  a    xy  -zx | | x |
+         * |  yw -xy   a    yz | | y |
+         * | -wz  zx  -yz   a  | | z | 
+         * | xyz ywz  zwx  wxy | */
         Vector4 m;
-        m.w = r.a * v.w + r.wx * v.x - r.yw * v.y + r.wz * v.z;
-        m.x = -r.wx * v.w + r.a * v.x + r.xy * v.y - r.zx * v.z;
-        m.y = r.yw * v.w - r.xy * v.x + r.a * v.y + r.yz * v.z;
-        m.z = -r.wz * v.w + r.zx * v.x - r.yz * v.y + r.a * v.z;
+        m.w =  r.a  * v.w + r.wx * v.x - r.yw * v.y + r.wz * v.z;
+        m.x = -r.wx * v.w + r.a  * v.x + r.xy * v.y - r.zx * v.z;
+        m.y =  r.yw * v.w - r.xy * v.x + r.a  * v.y + r.yz * v.z;
+        m.z = -r.wz * v.w + r.zx * v.x - r.yz * v.y + r.a  * v.z;
         float qv = tv.w * v.w + tv.x * v.x + tv.y * v.y + tv.z * v.z;
 
-        /* |  a   -wx   yw   wz  |
-         * |  wx   a   -xy   zx  |
-         * | -yw   xy   a   -yz  |
-         * |  wz  -zx   yz   a   |
-         * |  xyz -yzw -zwx  wxy |
-         */
+        /*                        |  a   -wx   yw   wz  |
+         *                        |  wx   a   -xy   zx  |
+         * | m.w m.x m.y m.z qv | | -yw   xy   a   -yz  |
+         *                        |  wz  -zx   yz   a   |
+         *                        |  xyz -yzw -zwx  wxy | */
         Vector4 n;
-        n.w = m.w * r.a + m.x * r.wx - m.y * r.yw + m.z * r.wz + qv * tv.w;
-        n.x = -m.w * r.wx + m.x * r.a + m.y * r.xy - m.z * r.zx - qv * tv.x;
-        n.y = m.w * r.yw - m.x * r.xy + m.y * r.a + m.z * r.yz - qv * tv.y;
-        n.z = -m.w * r.wz + m.x * r.zx - m.y * r.yz + m.z * r.a + qv * tv.z;
+        n.w =  m.w * r.a  + m.x * r.wx - m.y * r.yw + m.z * r.wz + qv * tv.w;
+        n.x = -m.w * r.wx + m.x * r.a  + m.y * r.xy - m.z * r.zx - qv * tv.x;
+        n.y =  m.w * r.yw - m.x * r.xy + m.y * r.a  + m.z * r.yz - qv * tv.y;
+        n.z = -m.w * r.wz + m.x * r.zx - m.y * r.yz + m.z * r.a  + qv * tv.z;
         return n;
     }
 
-    // constructs a Rotor to rotate from one vector to another
+    // constructs a rotor that performs whatever rotation takes the first vector to the second 
     static Rotor4 ConstructRotor(Vector4 vFrom, Vector4 vTo)
     {
         float a = 1f + Vector4.Dot(vTo, vFrom);
-
-        // left rotation is b * a so flip vectors
         Bivector4 b = Bivector4.Wedge(vTo, vFrom);
 
         return new Rotor4(a, b).normalized;
     }
-
-    Rotor4 RotorInverse(Rotor4 r) => new Rotor4(r.a, r.b * -1f);
 }
